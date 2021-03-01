91d54929b3ce18b6bc94e8551fe4b474a857652a
RohithChevvakula <45816813+RohithChevvakula@users.noreply.github.com>
2021-03-01 10:24:38 +0530

    Update PersonalLogin.java

--- src/test/java/com/makeMyTrip/e2e/PersonalLogin.java
+++ src/test/java/com/makeMyTrip/e2e/PersonalLogin.java
@@ -6,20 +6,24 @@ import java.util.Properties;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import junit.framework.TestCase;
 
 public class PersonalLogin extends TestCase{
-	 private WebDriver driver;
-	 private Properties properties = new Properties();
-	 protected WebDriverWait wait = new WebDriverWait(driver, 30);
+	  WebDriver driver;
+	  Properties properties = new Properties();
+	  WebDriverWait wait;
 	String userName = "", password = ""; boolean loginBoxEnabled = false;
+	By accountButton = By.xpath("//li[@data-cy='account']"), userNameField=By.xpath("//input[@id='username']"), continueButton =By.xpath("//span[text()='Continue']"), passwordField =By.xpath("//input[@type='password']"), loginButton=By.xpath("//span[text()='Login']"),loginBox = By.xpath("//div[@class='loginModal displayBlock modalLogin dynHeight personal ']"), loginCard=By.xpath("//div[@class='login__card makeFlex hrtlCenter cursorPointer appendBottom10']");
 
 	public void setUp() throws Exception {
 		properties.load(new FileReader(new File("src/test/resources/test.properties")));
 		System.setProperty("webdriver.chrome.driver", properties.getProperty("chromeDriver"));
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("https://www.makemytrip.com/");
 		userName = properties.getProperty("username");
 		password = properties.getProperty("password");
 	}
@@ -30,34 +34,31 @@ public class PersonalLogin extends TestCase{
 
 	@Test
 	public void test() throws Exception {
-		driver.get("https://www.makemytrip.com/");
-		Thread.sleep(2000);
 		try {
-			WebElement d = driver.findElement(By.xpath("//div[@class='loginModal displayBlock modalLogin dynHeight personal ']"));
+			WebElement d = find(loginBox);
 			if (d != null) {
-				driver.findElement(By.xpath("//div[@class='login__card makeFlex hrtlCenter cursorPointer appendBottom10']")).click();
+				find(loginCard).click();
 				loginBoxEnabled = true;
 			} 
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 			if(!loginBoxEnabled) {
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("//li[@data-cy='account']")).click();
+			find(accountButton).click();
 			}
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("//input[@id='username']")).sendKeys(userName);
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("//span[text()='Continue']")).click();
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
-			Thread.sleep(2000);
-			driver.findElement(By.xpath("//span[text()='Login']")).click();
-			Thread.sleep(2000);
-		
-//		<input type="text" class="font14 fullWidth" id="username" autocomplete="off" placeholder="Enter email or mobile number" data-cy="userName" value="">
-//		<input type="text" class="font14 fullWidth" id="username" autocomplete="off" placeholder="Enter email or mobile number" data-cy="userName" value="">
-
+			find(userNameField).sendKeys(userName);
+			
+			find(continueButton).click();
+			
+			find(passwordField).sendKeys(password);
+			
+			find(loginButton).click();
+			Thread.sleep(5000);
+	}
+	
+	WebElement find(By element){
+		wait = new WebDriverWait(driver, 15);
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(element));
 	}
 
 }
